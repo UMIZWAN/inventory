4848237e943aab3e474505e57ff1fd6a89e6522e
Izwan <umizwanmanda@gmail.com>
2025-04-17 16:44:11 +0800

    add new migration

--- database/migrations/2025_04_17_064844_create_assets_transaction_table.php
+++ database/migrations/2025_04_17_064844_create_assets_transaction_table.php
@@ -13,7 +13,30 @@ return new class extends Migration
     {
         Schema::create('assets_transaction', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('asset_id')->constrained('assets')->cascadeOnDelete();
+            $table->foreignId('users_id')->constrained('users')->cascadeOnDelete();
+            $table->string('assets_transaction_description');
             $table->timestamps();
+        });
+
+        Schema::table('transaction_list', function (Blueprint $table) {
+            $table->id();
+            $table->string('assets_transaction_running_number')->nullable();
+            $table->foreignId('asset_transaction_id')->constrained('assets_transaction')->cascadeOnDelete();
+            $table->string('assets_transaction_type')->default('ASSET IN');
+            $table->string('assets_transaction_status')->default('PENDING');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->dateTime('created_at')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->dateTime('updated_at')->nullable();
+            $table->unsignedBigInteger('received_by')->nullable();
+            $table->dateTime('received_at')->nullable();
+            $table->unsignedBigInteger('approved_by')->nullable();
+            $table->dateTime('approved_at')->nullable();
+            $table->foreign('created_by')->references('id')->on('users')->nullOnDelete();
+            $table->foreign('updated_by')->references('id')->on('users')->nullOnDelete();
+            $table->foreign('received_by')->references('id')->on('users')->nullOnDelete();
+            $table->foreign('approved_by')->references('id')->on('users')->nullOnDelete();
         });
     }
 
