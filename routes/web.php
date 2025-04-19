4b5cfe7508d595cb3fc424f047bc1dda20aa08ea
kamaleiah <klmnop.558@gmail.com>
2025-04-19 11:26:58 +0800

    setup vite, react, inertia

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('tests.index');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return inertia('Login');
 });
+
+
+Route::get('/assets', function () {
+    return inertia('Assets');
+});
+
+
