f7e2d6976bd985ce5c77ab3b2de91359aa94caf1
Izwan <umizwanmanda@gmail.com>
2025-05-07 17:13:28 +0800

    add observer

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Cache;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        Cache::forget('users_cache');
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        Cache::forget('users_cache');
+    }
+}
