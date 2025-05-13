d117201d014537f4b87f5992fb39e60bbacd0dce
Izwan <umizwanmanda@gmail.com>
2025-05-13 14:29:10 +0800

    si kay lupa auth context

--- resources/js/context/AuthContext.jsx
+++ resources/js/context/AuthContext.jsx
@@ -54,7 +54,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, login, logout, loading }}>
+    <AuthContext.Provider value={{ user, login, logout, loading, fetchUser }}>
       {children}
     </AuthContext.Provider>
   );
