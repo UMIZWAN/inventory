b5d22a2d9e2f144b7c66dd906b4f2d68e22f1d23
Izwan <umizwanmanda@gmail.com>
2026-03-13 09:19:45 +0800

    puah

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,26 +16,16 @@ class Cors
         $response = $next($request);
 
         if (!$request->isMethod('OPTIONS')) {
-<<<<<<< HEAD
-            // $response->headers->set('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
-            $response->headers->set('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
-=======
             $response->headers->set('Access-Control-Allow-Origin', 'http://127.0.0.1:8081');
             // $response->headers->set('Access-Control-Allow-Origin', 'https://inventory.umgroup.com.my');
->>>>>>> b7bff0947af8e0bc0f6477a22d01db502479db3e
             $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
             $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN');
             $response->headers->set('Access-Control-Allow-Credentials', 'true');
         } else {
             // Handle preflight OPTIONS request
             $response = response('', 200);
-<<<<<<< HEAD
-            // $response->headers->set('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
-            $response->headers->set('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
-=======
             $response->headers->set('Access-Control-Allow-Origin', 'http://127.0.0.1:8081');
             // $response->headers->set('Access-Control-Allow-Origin', 'https://inventory.umgroup.com.my');
->>>>>>> b7bff0947af8e0bc0f6477a22d01db502479db3e
             $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
             $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN');
             $response->headers->set('Access-Control-Allow-Credentials', 'true');
